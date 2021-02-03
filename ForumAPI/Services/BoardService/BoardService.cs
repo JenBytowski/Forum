f281e2776d8ba50ce99c11e91d461bb7e6717bb8
Yuheni Tabakerov <yuheni_tabakerov@strong-code.pro>
2021-02-03 23:42:56 +0300

    add IboardServiceInterface and emplement methods

--- ForumAPI/Services/BoardService/BoardService.cs
+++ ForumAPI/Services/BoardService/BoardService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using ForumAPI.Services.BoardService.BoardDataContext;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +10,11 @@ namespace ForumAPI.Services.BoardService
 {
     public interface IBoardService
     {
-        public IList<BoardModel> GetBoards();
+        IEnumerable<BoardModel> GetBoards();
+
+        Task<IEnumerable<TopicModel>> GetTopicsByBoard(string board);
     }
-    
+
     internal sealed class BoardService : IBoardService
     {
         private BoardContext boardContext;
@@ -20,69 +24,23 @@ namespace ForumAPI.Services.BoardService
             this.boardContext = boardContext;
         }
 
-        public IList<BoardModel> GetBoards()
+        public IEnumerable<BoardModel> GetBoards()
         {
-            var boards = boardContext.Boards.Include(brd => brd.Topics).ThenInclude(top => top.Posts)
-                .ThenInclude(pst => pst.AdditionalPostInfos);
-            var boardModels = boards.Select(brd => MapToModel(brd)).ToList();
+            this.boardContext.Boards.Load();
+            var boards = this.boardContext.Boards.Local.Select(brd => BoardModel.MapToModel(brd));
 
-            return boardModels;
+            return boards;
         }
 
-        private static BoardModel MapToModel(Board board)
+        public async Task<IEnumerable<TopicModel>> GetTopicsByBoard(string board)
         {
-            return new BoardModel
-            {
-                Id = board.Id,
-                Name = board.Name,
-                Topics = board.Topics.Select(top => new TopicModel
-                {
-                    Id = top.Id,
-                    CreaterId = top.CreaterId,
-                    TopicHeader = top.TopicHeader,
-                    Posts = top.Posts.Select(pst => new PostModel
-                    {
-                        Id = pst.Id,
-                        PosterId = pst.PosterId,
-                        Text = pst.Text,
-                        AdditionalPostInfos = pst.AdditionalPostInfos.Select(inf => new AdditionalPostInfoModel
-                        {
-                            Id = inf.Id,
-                            ContentURL = inf.ContentURL
-                        }).ToList()
-                    }).ToList()
-                }).ToList()
-            };
+            var topics = this.boardContext.Topics.Include(tp => tp.Board).Include(tp => tp.Posts).ThenInclude(pst => pst.AdditionalPostInfos);
+            var boardTopics = topics.Where(tp => tp.Board.Name == board).Select(tp => TopicModel.MapToModel(tp));
+
+            return boardTopics;
         }
 
-        private static Board MapFromModel(BoardModel model)
-        {
-            return new Board
-            {
-                Id = model.Id,
-                Name = model.Name,
-                Topics = model.Topics.Select(top => new Topic
-                {
-                    Id = top.Id,
-                    BoardId = model.Id,
-                    CreaterId = top.CreaterId,
-                    TopicHeader = top.TopicHeader,
-                    Posts = top.Posts.Select(pst => new Post
-                    {
-                        Id = pst.Id,
-                        TopicId = top.Id,
-                        PosterId = pst.PosterId,
-                        Text = pst.Text,
-                        AdditionalPostInfos = pst.AdditionalPostInfos.Select(inf => new AdditionalPostInfo
-                        {
-                            Id = inf.Id,
-                            PostId = pst.Id,
-                            ContentURL = inf.ContentURL
-                        }).ToList()
-                    }).ToList()
-                }).ToList()
-            };
-        }
+        
     }
 
     public sealed class BoardModel
@@ -92,34 +50,128 @@ namespace ForumAPI.Services.BoardService
         public string Name { get; set; }
 
         public IList<TopicModel> Topics { get; set; }
+        
+        internal static BoardModel MapToModel(Board board)
+        {
+            return new BoardModel
+            {
+                Id = board.Id,
+                Name = board.Name,
+                Topics = board.Topics?.Select(top => TopicModel.MapToModel(top)).ToList()
+            };
+        }
+
+        internal static Board MapFromModel(BoardModel model)
+        {
+            return new Board
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Topics = model.Topics.Select(top => TopicModel.MapFromModel(top)).ToList()
+            };
+        }
     }
 
     public sealed class TopicModel
     {
         public Guid Id { get; set; }
 
+        public Guid BoardId { get; set; }
+
         public Guid CreaterId { get; set; }
 
         public string TopicHeader { get; set; }
 
         public IList<PostModel> Posts { get; set; }
+
+        internal static TopicModel MapToModel(Topic topic)
+        {
+            return new TopicModel()
+            {
+                Id = topic.Id,
+                BoardId = topic.BoardId,
+                TopicHeader = topic.TopicHeader,
+                CreaterId = topic.CreaterId,
+                Posts = topic.Posts?.Select(pst => PostModel.MapToModel(pst)).ToList()
+            };
+        }
+
+        internal static Topic MapFromModel(TopicModel model)
+        {
+            return new Topic
+            {
+                Id = model.Id,
+                BoardId = model.BoardId,
+                TopicHeader = model.TopicHeader,
+                CreaterId = model.CreaterId,
+                Posts = model.Posts?.Select(pst => PostModel.MapFromModel(pst)).ToList()
+            };
+        }
     }
 
     public sealed class PostModel
     {
         public Guid Id { get; set; }
 
+        public Guid TopicId { get; set; }
+
         public Guid PosterId { get; set; }
 
         public string Text { get; set; }
 
         public IList<AdditionalPostInfoModel> AdditionalPostInfos { get; set; }
+
+        internal static PostModel MapToModel(Post post)
+        {
+            return new PostModel
+            {
+                Id = post.Id,
+                TopicId = post.TopicId,
+                PosterId = post.PosterId,
+                Text = post.Text,
+                AdditionalPostInfos = post.AdditionalPostInfos?.Select(inf => AdditionalPostInfoModel.MapToModel(inf)).ToList()
+            };
+        }
+
+        internal static Post MapFromModel(PostModel model)
+        {
+            return new Post
+            {
+                Id = model.Id,
+                TopicId = model.TopicId,
+                PosterId = model.PosterId,
+                Text = model.Text,
+                AdditionalPostInfos = model.AdditionalPostInfos?.Select(inf => AdditionalPostInfoModel.MapFromModel(inf)).ToList()
+            };
+        }
     }
 
     public sealed class AdditionalPostInfoModel
     {
         public Guid Id { get; set; }
 
+        public Guid PostId { get; set; }
+
         public string ContentURL { get; set; }
+
+        internal static AdditionalPostInfoModel MapToModel(AdditionalPostInfo additionalPostInfo)
+        {
+            return new AdditionalPostInfoModel
+            {
+                Id = additionalPostInfo.Id,
+                PostId = additionalPostInfo.PostId,
+                ContentURL = additionalPostInfo.ContentURL
+            };
+        }
+        
+        internal static AdditionalPostInfo MapFromModel(AdditionalPostInfoModel additionalPostInfo)
+        {
+            return new AdditionalPostInfo
+            {
+                Id = additionalPostInfo.Id,
+                PostId = additionalPostInfo.PostId,
+                ContentURL = additionalPostInfo.ContentURL
+            };
+        }
     }
 }
